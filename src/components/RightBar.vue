060bdc9055b036507e731f40edc0d8ac371dc731
Mitchell-Mashburn <mitchell@re3d.org>
2025-02-04 16:18:47 -0600

    Add rightsidebar

---
+++ src/components/RightBar.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="right-sidebar">
+      <h3>Right Sidebar</h3>
+      <p>Test Content</p>
+    </div>
+</template>
+
+<style scoped>
+.right-sidebar {
+  width: 300px;
+  background-color: #242527;
+  height: 100%;
+  border-left: 1px solid #ccc;
+  padding: 16px;
+}
+</style>
